0a3d67e5f6758041f2da858b253bb135f3561443
Dongying <1330332817@qq.com>
2021-01-22 18:51:37 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,27 @@
+#include "Sync_inter.h"
 
+int main()
+{
+    std::vector<double> cam_t;
+    std::vector<double> imu_t; 
+    std::vector<Q> imu_q;
+
+    GenTimestampQuaternion( cam_t, imu_t, imu_q);
+
+    Q qc;
+    for (const auto& tc : cam_t)
+    {
+        if (CalImuPose(tc, imu_t, imu_q, qc))
+        {
+            std::cout << "qc : "
+                      << qc.x << "  "
+                      << qc.y << "  "
+                      << qc.z << "  "
+                      << qc.w << "  "
+                      << std::endl;
+        }
+        
+    }
+    
+    return 0;
+}
